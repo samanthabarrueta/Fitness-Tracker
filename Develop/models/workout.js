25938b8c8b5231fff9d28d780656b0c618b91bcf
Samantha <samanthabarrueta@gmail.com>
2020-08-02 21:31:44 -0500

    workout schema

---
+++ Develop/models/workout.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: () => new Date()
+        },
+        exercises: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: 'Exercise type is required'
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: 'Exercise name is required'
+                },
+                duration: {
+                    type: Number,
+                    required: 'Exercise duration is required'
+
+                },
+                weight: {
+                    type: Number
+                },
+                reps: {
+                    type: Number
+                },
+                sets: {
+                    type: Number
+                }, 
+                distance: {
+                    type: Number
+                }
+            }
+        ]
+    }
+);
+
+const Workout = mongoose.model("Workout", WorkoutSchema)
+
+module.exports = Workout;
